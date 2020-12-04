50cdf04760337da930516f1146a80ba6dd75a68c
HenriDellal <anridellal@gmail.com>
2020-12-04 03:59:49 +0300

    kconfig check: add Samsung-specific options (MR 2000)
    Adds checks for following kernel config options:
    SAMSUNG_TUI:
        TUI HW Handler - related to Samsung's security measures
        Creates "secure frame buffer", results in bootloop
    SEC_RESTRICT_ROOTING:
        blocks gaining root permissions
    TZDEV:
        Samsung TZ Based Secure OS interface driver
        (results in bootloops)

--- pmb/config/__init__.py
+++ pmb/config/__init__.py
@@ -199,7 +199,10 @@ necessary_kconfig_options = {
             "EXT4_FS": True,
             "KINETO_GAN": False,
             "PFT": False,
+            "SAMSUNG_TUI": False,
+            "SEC_RESTRICT_ROOTING": False,
             "SYSVIPC": True,
+            "TZDEV": False,
             "USE_VFB": False,
             "VT": True,
         }
