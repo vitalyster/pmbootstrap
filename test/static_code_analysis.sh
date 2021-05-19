1704851468b05b954f6d15d2f96b553f6934f7a5
Caio Fontes <caiofcastro@usp.br>
2021-05-19 16:48:03 -0300

    test/static_code_analysis.sh: update E501 check (MR 2058)
    Change from checking some files for E501 to excluding some files from
    checking.

--- test/static_code_analysis.sh
+++ test/static_code_analysis.sh
@@ -47,119 +47,16 @@ $flake8_command --exclude=__init__.py --ignore "$_ignores" $py_files
 $flake8_command --filename=__init__.py --ignore "F401,$_ignores" $py_files
 
 # Enforce max line length of 79 characters (#1986). We are iteratively fixing
-# up the source files to adhere to this rule, so only check the ones that were
-# fixed for now. Eventually, E501 can be removed from _ignores above, and this
+# up the source files to adhere to this rule, so ignore the ones that are not
+# yet fixed. Eventually, E501 can be removed from _ignores above, and this
 # whole block can be removed.
 echo "Test with flake8: *.py (E501)"
-py_files="
-	.gitlab/check_mr_settings.py
-	pmb/aportgen/device.py
-	pmb/build/__init__.py
-	pmb/build/_package.py
-	pmb/build/autodetect.py
-	pmb/build/checksum.py
-	pmb/build/envkernel.py
-	pmb/build/init.py
-	pmb/build/menuconfig.py
-	pmb/build/newapkbuild.py
-	pmb/build/other.py
-	pmb/chroot/__init__.py
-	pmb/chroot/apk.py
-	pmb/chroot/apk_static.py
-	pmb/chroot/binfmt.py
-	pmb/chroot/distccd.py
-	pmb/chroot/init.py
-	pmb/chroot/initfs.py
-	pmb/chroot/initfs_hooks.py
-	pmb/chroot/mount.py
-	pmb/chroot/other.py
-	pmb/chroot/root.py
-	pmb/chroot/shutdown.py
-	pmb/chroot/user.py
-	pmb/chroot/zap.py
-	pmb/config/pmaports.py
-	pmb/config/save.py
-	pmb/config/workdir.py
-	pmb/export/__init__.py
-	pmb/export/frontend.py
-	pmb/export/symlinks.py
-	pmb/flasher/__init__.py
-	pmb/helpers/__init__.py
-	pmb/helpers/apk.py
-	pmb/helpers/aportupgrade.py
-	pmb/helpers/args.py
-	pmb/helpers/file.py
-	pmb/helpers/git.py
-	pmb/helpers/lint.py
-	pmb/helpers/mount.py
-	pmb/helpers/package.py
-	pmb/helpers/repo.py
-	pmb/helpers/repo_missing.py
-	pmb/helpers/run.py
-	pmb/helpers/status.py
-	pmb/helpers/ui.py
-	pmb/install/__init__.py
-	pmb/install/_install.py
-	pmb/install/blockdevice.py
-	pmb/install/format.py
-	pmb/install/losetup.py
-	pmb/install/partition.py
-	pmb/install/recovery.py
-	pmb/install/ui.py
-	pmb/parse/__init__.py
-	pmb/parse/_apkbuild.py
-	pmb/parse/apkindex.py
-	pmb/parse/arch.py
-	pmb/parse/arguments.py
-	pmb/parse/binfmt_info.py
-	pmb/parse/bootimg.py
-	pmb/parse/cpuinfo.py
-	pmb/parse/depends.py
-	pmb/parse/deviceinfo.py
-	pmb/parse/kconfig.py
-	pmb/parse/version.py
-	pmb/qemu/__init__.py
-	pmb/sideload/__init__.py
-	pmbootstrap.py
-	test/pmb_test/__init__.py
-	test/pmb_test/const.py
-	test/pmb_test/git.py
-	test/test_aportgen.py
-	test/test_bootimg.py
-	test/test_config_init.py
-	test/test_config_pmaports.py
-	test/test_config_workdir.py
-	test/test_cross_compile_distcc.py
-	test/test_crossdirect.py
-	test/test_envkernel.py
-	test/test_file.py
-	test/test_folder_size.py
-	test/test_frontend.py
-	test/test_helpers_git.py
-	test/test_helpers_lint.py
-	test/test_helpers_package.py
-	test/test_helpers_pmaports.py
-	test/test_helpers_repo.py
-	test/test_helpers_repo_missing.py
-	test/test_helpers_status.py
-	test/test_helpers_ui.py
-	test/test_install.py
-	test/test_kconfig_check.py
-	test/test_keys.py
-	test/test_mount.py
-	test/test_newapkbuild.py
-	test/test_parse_apkbuild.py
-	test/test_parse_apkindex.py
-	test/test_parse_depends.py
-	test/test_parse_deviceinfo.py
-	test/test_questions.py
-	test/test_run_core.py
-	test/test_shell_escape.py
-	test/test_version.py
-	test/test_version_validate.py
-"
+exc_files="./pmb/aportgen/busybox_static.py"
+exc_files="$exc_files,./pmb/aportgen/grub_efi.py"
+exc_files="$exc_files,./pmb/aportgen/linux.py"
+exc_files="$exc_files,./pmb/aportgen/musl.py"
 # shellcheck disable=SC2086
-$flake8_command --select="E501" $py_files
+$flake8_command --select="E501" --exclude=$exc_files $py_files
 
 # Done
 echo "Success!"
