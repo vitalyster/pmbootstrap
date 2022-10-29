4771fbac6547223708318f323ca6c05051274864
Luca Weiss <luca@z3ntu.xyz>
2022-10-29 13:45:36 +0200

    pmb.qemu.run: support riscv64
    We're using the standard virt machine, adding virtio-gpu-pci for
    graphics and are using the bios firmware that is installed from the
    device package, instead of the one built-in to QEMU.
    
    Reviewed-by: Oliver Smith <ollieparanoid@postmarketos.org>
    Link: https://lists.sr.ht/~postmarketos/pmbootstrap-devel/%3C20221029114536.100268-2-luca@z3ntu.xyz%3E

--- pmb/qemu/run.py
+++ pmb/qemu/run.py
@@ -193,6 +193,10 @@ def command_qemu(args, arch, img_path, img_path_2nd=None):
         command += ["-M", "virt"]
         command += ["-cpu", "cortex-a57"]
         command += ["-device", "virtio-gpu-pci"]
+    elif arch == "riscv64":
+        command += ["-M", "virt"]
+        command += ["-device", "virtio-gpu-pci"]
+        command += ["-bios", f"{rootfs}/usr/share/opensbi/generic/firmware/fw_jump.elf"]
     else:
         raise RuntimeError(f"Architecture {arch} not supported by this command"
                            " yet.")
